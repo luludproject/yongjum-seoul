2a255c6f19a531aef4e5e38a362e77186cb9a8e6
unknown <chlalstn9@gmail.com>
2024-07-24 04:18:26 +0900

    update

---
+++ js/main.js
@@ -0,0 +1,181 @@
+(function ($) {
+  "use strict";
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 30) {
+      $("#nav").addClass("nav-sticky");
+    } else {
+      $("#nav").removeClass("nav-sticky");
+    }
+  });
+  if ($(window).scrollTop() > 30) {
+    $("#nav").addClass("nav-sticky");
+  }
+
+  // Smooth scrolling on the navbar links
+  $(".navbar-nav a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      $("html, body").animate(
+        {
+          scrollTop: $(this.hash).offset().top - 45,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+
+      if ($(this).parents(".navbar-nav").length) {
+        $(".navbar-nav .active").removeClass("active");
+        $(this).closest("a").addClass("active");
+      }
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Testimonial Slider
+  $(".testimonial-slider").slick({
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".testimonial-slider-nav",
+  });
+  $(".testimonial-slider-nav").slick({
+    arrows: false,
+    dots: false,
+    focusOnSelect: true,
+    centerMode: true,
+    centerPadding: "22px",
+    slidesToShow: 3,
+    asNavFor: ".testimonial-slider",
+  });
+  $("#testimonial .slider-nav").css({ position: "relative", height: "120px" });
+})(jQuery);
+
+(function ($) {
+  "use strict";
+
+  // Fixed Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-200px");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Latest-news-carousel
+  $(".latest-news-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  // What's New carousel
+  $(".whats-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+  });
+
+  // Modal Video
+  $(document).ready(function () {
+    var $videoSrc;
+    $(".btn-play").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
+
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#video").attr("src", $videoSrc);
+    });
+  });
+})(jQuery);
